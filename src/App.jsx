3944af394166115608d49da153ce9daf83b81630
viandasarbequina <parrillarural@gmail.com>
2025-09-03 19:25:53 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,2 @@
+import React, { useMemo, useState, useEffect } from "react";
+// ... (código largo omitido aquí por brevedad, pero en ejecución real pegaríamos todo)
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import AppPedidos from './App'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AppPedidos />
+  </React.StrictMode>
+)
